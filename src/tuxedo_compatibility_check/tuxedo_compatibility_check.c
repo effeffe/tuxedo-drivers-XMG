689bae21b9e0c4e5086af0c94dfb7aa8f67221f3
Filippo Falezza <filippo.falezza@outlook.it>
2024-04-29 15:10:10 +0100

    Replaced with XMG/Schenker dmi strings

--- src/tuxedo_compatibility_check/tuxedo_compatibility_check.c
+++ src/tuxedo_compatibility_check/tuxedo_compatibility_check.c
@@ -129,17 +129,17 @@ static const struct x86_cpu_id force_tuxedo_dmi_string_check_match[] = {
 static const struct dmi_system_id tuxedo_dmi_string_match[] = {
 	{
 		.matches = {
-			DMI_MATCH(DMI_SYS_VENDOR, "TUXEDO"),
+			DMI_MATCH(DMI_SYS_VENDOR, "SchenkerTechnologiesGmbH"),
 		},
 	},
 	{
 		.matches = {
-			DMI_MATCH(DMI_BOARD_VENDOR, "TUXEDO"),
+			DMI_MATCH(DMI_BOARD_VENDOR, "NB02"),
 		},
 	},
 	{
 		.matches = {
-			DMI_MATCH(DMI_CHASSIS_VENDOR, "TUXEDO"),
+			DMI_MATCH(DMI_CHASSIS_VENDOR, "SchenkerTechnologiesGmbH"),
 		},
 	},
 	{ }
